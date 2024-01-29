324d66ff4c1370439495d4172599902bcc54111c
Joseph Reedy <jreedy@mines.edu>
2024-01-28 17:46:26 -0700

    setting class

--- C06C-Stove/src/Burner.java
+++ C06C-Stove/src/Burner.java
@@ -1,3 +1,11 @@
 public class Burner {
-    
+    private enum Temperature {
+        BLAZING, HOT, WARM, COLD;
+    }
+   private Temperature myTemperature;
+   private Setting mySetting;
+
+   public Temperature get() {
+        return myTemperature;
+   }
 }
