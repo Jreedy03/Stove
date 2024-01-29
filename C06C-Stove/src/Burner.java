5ad51f22e44e76611394345ae359e66672cdd68a
Joseph Reedy <jreedy@mines.edu>
2024-01-28 22:50:43 -0700

    asd

--- C06C-Stove/src/Burner.java
+++ C06C-Stove/src/Burner.java
@@ -1,6 +1,6 @@
 public class Burner {
     private enum Temperature {
-        BLAZING, HOT, WARM, COLD;
+        COLD, WARM, HOT, BLAZING;
     }
    private Temperature myTemperature;
    private Setting mySetting;
@@ -78,14 +78,49 @@ public class Burner {
         }
     }
 
-    public void updateTemperature() {
-        timer--;
-        if (timer == 0) {
-            increaseTemp();
+
+    public void decreaseTemp() {
+        switch (myTemperature) {
+            case BLAZING:
+                if (mySetting != Setting.HIGH) {
+                    myTemperature = Temperature.HOT;
+                    timer = TIME_DURATION;
+                }
+                break;
+            case HOT:
+                if (mySetting != Setting.MEDIUM) {
+                    myTemperature = Temperature.WARM;
+                    timer = TIME_DURATION;
+                }
+                break;
+            case WARM:
+                if (mySetting != Setting.LOW) {
+                    myTemperature = Temperature.COLD;
+                }
+                break;
+            case COLD:
+                break;
         }
     }
+
+    public void updateTemperature() {
+        if (timer > 0) {
+            timer--;
+        }
+
+        if (timer == 0) {
+            if (mySetting.ordinal() > myTemperature.ordinal()) {
+            
+                increaseTemp();
+            } else if (mySetting.ordinal() < myTemperature.ordinal()) {
+                
+                decreaseTemp();
+            }
     
-    public void display(Burner burner) {
-    	System.out.println(burner.mySetting.toString() + " ..... " + burner.get());
+        }
     }
+
+public void display(Burner burner) {
+    System.out.println(burner.mySetting.toString() + " ..... " + burner.get());
+}
 }
